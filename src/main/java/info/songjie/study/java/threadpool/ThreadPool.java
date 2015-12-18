c8dedad27aa9b3adc4f8932f72d12901038e6aef
songjie <songjiee@abchina.con>
2015-12-18 10:37:26 +0800

    实现基本的线程池功能

--- src/main/java/info/songjie/study/java/threadpool/ThreadPool.java
+++ src/main/java/info/songjie/study/java/threadpool/ThreadPool.java
@@ -1,5 +1,73 @@
 package info.songjie.study.java.threadpool;
 
-public final class ThreadPool {
+import java.util.LinkedList;
+import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public final class ThreadPool {
+	private Logger logger = LoggerFactory.getLogger(ThreadPool.class);
+	// 线程池的初始大小
+	private int initSize = 5;
+	
+	// 工作线程
+	private WorkThread[] workThreads;
+	
+	// 是否继续接受新的任务
+	private boolean canReceiveNewTask = true;
+	
+	// 任务列表。非线程安全的，对其进行操作要注意同步。
+	List<Runnable> taskQuene = new LinkedList<Runnable>();
+	
+	// 执行完成的任务数
+	private long finishedTask = 0;
+	
+	public ThreadPool(){
+		this(5);
+	}
+	
+	public ThreadPool(int initSize){
+		workThreads = new WorkThread[initSize];
+		for(int i=0; i<initSize; i++){
+			workThreads[i] = new WorkThread();
+			new Thread(workThreads[i]).start();
+		}
+	}
+	
+	public void execute(Runnable task){
+		synchronized(taskQuene){
+			taskQuene.add(task);
+			taskQuene.notify();
+		}
+	}
+	
+	// 工作线程被定义为私有内部类
+	private class WorkThread implements Runnable{
+		// 该线程是否能够工作，将其置为false则会停止该线程
+		private boolean canWork  = true;
+		public void run() {
+			while(canWork){
+				Runnable task = null;
+				synchronized(taskQuene){
+					logger.debug("共有{}个任务", taskQuene.size());
+					if(!taskQuene.isEmpty()){
+						if(!taskQuene.isEmpty()){
+							task = taskQuene.remove(0);
+						}
+					}else{
+						try {
+							taskQuene.wait();
+						} catch (InterruptedException e) {
+							e.printStackTrace();
+						}
+					}
+				}
+				if(task != null){
+					task.run();
+					finishedTask++;
+				}
+			}
+		}
+	}
 }
--- src/test/java/info/songjie/study/java/threadpool/ThreadPoolTest.java
+++ src/test/java/info/songjie/study/java/threadpool/ThreadPoolTest.java
@@ -1,7 +1,38 @@
 package info.songjie.study.java.threadpool;
 
+import java.io.IOException;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import junit.framework.TestCase;
 
 public class ThreadPoolTest extends TestCase {
-
+	private static Logger logger = LoggerFactory.getLogger(ThreadPoolTest.class);
+	
+	@Test
+	public void testThreadPool() throws IOException{
+		ThreadPool threadPool = new ThreadPool();
+		for(int i=0; i<100; i++){
+			threadPool.execute(new Task());
+		}
+		System.in.read();
+	}
+	
+	
+	private static class Task implements Runnable {  
+        private volatile static  int i = 1;  
+  
+        public void run() {// 执行任务  
+        	try {
+				Thread.sleep((long) (10));
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+        	logger.debug("任务{}完成", i);
+        	i++;
+        }  
+    }  
 }
